eaa1c014ea5459473498edacd6a2d3fd6459c514
arshjit pelia <arsh.pelia1@gmail.com>
2023-09-23 15:55:02 -0400

    spacing

--- src/search/main.py
+++ src/search/main.py
@@ -9,6 +9,7 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption('A* Path Finding Algo')
 
+
 class State:
     """ Different states a node may be in """
     NORMAL = '#0f042b'
@@ -20,6 +21,7 @@ class State:
     VISITING = '#fc03c6'
     CAME_FROM = '#83fce0'
     PATH = '#f2fc83'
+
 
 class Node:
     def __init__(self, row, col, width, rows_tot):
@@ -37,27 +39,28 @@ class Node:
 
     def __repr__(self):
         return '<Node {}, State \'{}\'>'.format((self.row, self.col), self.state.name)
-    
+
     def __lt__(self, other):
         return False
 
     def is_barrier(self):
         return self.state == State.BARRIER
-    
+
     def reset(self):
         self.state = State.NORMAL
 
     @property
     def state(self):
         return self._state
-    
+
     @state.setter
     def state(self, state):
         self._state = state
-    
+
     def draw(self, win):
-        pygame.draw.rect(win, self.state, (self.x, self.y, self.width, self.width))
-    
+        pygame.draw.rect(
+            win, self.state, (self.x, self.y, self.width, self.width))
+
     def update_neighbors(self, grid):
         self.neighbors = []
         if self.row < self.rows_tot - 1 and not grid[self.row + 1][self.col].is_barrier():
@@ -72,10 +75,12 @@ class Node:
         if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
             self.neighbors.append(grid[self.row][self.col - 1])
 
+
 def manhattan(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
 
 def make_grid(rows, width):
     grid = np.empty((rows, rows), dtype=Node)
@@ -86,6 +91,7 @@ def make_grid(rows, width):
             grid[i][j] = n
     return grid
 
+
 def draw_grid(win, rows, width):
     gap = width // rows
     # grey = (128, 128, 128)
@@ -94,6 +100,7 @@ def draw_grid(win, rows, width):
         pygame.draw.line(win, line_col, (0, i * gap), (width, i * gap))
         for j in range(rows):
             pygame.draw.line(win, line_col, (j * gap, 0), (j * gap, width))
+
 
 def draw(win, grid, rows, width):
     win.fill('#f6f4f2')
@@ -105,6 +112,7 @@ def draw(win, grid, rows, width):
     draw_grid(win, rows, width)
     pygame.display.update()
 
+
 def get_clicked_pos(pos, rows, width):
     gap = width // rows
     y, x = pos
@@ -114,6 +122,7 @@ def get_clicked_pos(pos, rows, width):
 
     return row, col
 
+
 def reconstruct_path(came_from, current, draw):
     while current in came_from and current != None:  # Add "and current != None" to the condition
         current = came_from[current]
@@ -122,6 +131,7 @@ def reconstruct_path(came_from, current, draw):
         if current.state != State.START and current.state != State.FINISH:
             current.state = State.PATH
         draw()
+
 
 def bfs(draw, start, end):
     print('BFS')
@@ -148,8 +158,9 @@ def bfs(draw, start, end):
                 n.state = State.CAME_FROM
                 draw()
                 que.put((cur_cost + 1, n))
-    
+
     return False
+
 
 def dfs(draw, current, end, came_from):
     if current == end:
@@ -168,15 +179,18 @@ def dfs(draw, current, end, came_from):
 
     return False
 
+
 def dfs_search(draw, start, end):
     came_from = {}
     came_from[start] = None
     return dfs(draw, start, end, came_from)
 
+
 def heuristic(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
 
 def a_star(draw, grid, start, end):
     count = 0
@@ -207,7 +221,8 @@ def a_star(draw, grid, start, end):
             if temp_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score + heuristic(neighbor.get_pos(), end.get_pos())
+                f_score[neighbor] = temp_g_score + \
+                    heuristic(neighbor.get_pos(), end.get_pos())
                 if neighbor not in [node[2] for node in open_set.queue]:
                     count += 1
                     open_set.put((f_score[neighbor], count, neighbor))
@@ -219,6 +234,7 @@ def a_star(draw, grid, start, end):
             current.state = State.VISITING
 
     return False
+
 
 def display_controls_popup():
     root = tk.Tk()
@@ -235,6 +251,7 @@ def display_controls_popup():
     - Esc key: Quit the program
     """)
 
+
 def main(win, width):
     pygame.font.init()
     ROWS = 50
@@ -249,7 +266,7 @@ def main(win, width):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
+
             if pygame.mouse.get_pressed()[0]:  # LEFT-CLICK
                 pos = pygame.mouse.get_pos()
                 row, col = get_clicked_pos(pos, ROWS, width)
@@ -285,20 +302,22 @@ def main(win, width):
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
-                    dfs_search(lambda: draw(win, grid, ROWS, width), start, end)
+                    dfs_search(lambda: draw(
+                        win, grid, ROWS, width), start, end)
 
                 if event.key == pygame.K_a and start and end:  # a-key = A* search
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
                     a_star(lambda: draw(win, grid, ROWS, width), grid, start, end)
-                
+
                 if event.key == pygame.K_c:  # c-key = clear
                     start = None
                     end = None
                     grid = make_grid(ROWS, width)
 
     pygame.quit()
+
 
 if __name__ == '__main__':
     display_controls_popup()
